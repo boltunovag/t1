5905ce6208f2db5ea02cf27ba9afc9a4197cd4e1
Болтунов Алексей Геньевич <116594934+boltunovag@users.noreply.github.com>
2022-12-18 23:59:29 +0300

    Дошёл до букинга

--- js/script.js
+++ js/script.js
@@ -9,8 +9,6 @@ function toDay() {
   return ToDay;
 }
 function pr1() {
-  // alert($('.my_opts').css('display'));
-  /* $('.my_opts').css('display','inline-block');*/
 
   if ($('.my_opts').css('display') !== 'none') {
     $('.my_opts').css('display', 'none');
@@ -63,17 +61,18 @@ function check_kol() {
   else return true;
 }
 
-//Подводим итог
+//Кнопка отправки данных
 function button_func() {
   if (check_select() && check_cal() && check_kol()) {
     StrRegion = $('#myselect').text();
-    document.location = 'seat.html?' + send_kod(StrRegion);
+
+    document.location = 'seat.html?' + 'loc=' + send_kod(StrRegion) + '&kol=' + $('#inp_kol').val() + '&cal=' + $('#inp_cal').val();
   }
 }
 function send_kod(z) {
   if (z == "Алтайский край") return 0;
   else return 1;
-
+  /*******************//////////////************************************************/
 }
 //на странице seat узнаём какой регион выбран
 function receive_kod(y) {
@@ -85,4 +84,53 @@ function receive_kod(y) {
 
   return { Regh2, Adrp }
 }
+$(document).ready(function () {
 
+  if (location.pathname == '/seat.html') {
+    var paramsString = document.location.search;
+    var searchParams = new URLSearchParams(paramsString);
+    let kod_loc = searchParams.get("loc");//Получили код региона
+    let kol_chel = searchParams.get("kol");//получили количество человек
+    let cal_inp = searchParams.get("cal");
+    let imya_regiona = receive_kod(kod_loc).Regh2;//получили имя выбранного региона
+    $('#NRegion').text(imya_regiona);
+    $('#myselect').text(imya_regiona);//Вставили имя региона в инпут
+    $('#adres_loc').text(receive_kod(kod_loc).Adrp);
+
+    document.querySelector("#plan_loc").src = "images/plan/" + kod_loc + ".jpg";
+    //заполняем количество  делаем доступными зоны
+    if (Number(kol_chel) != NaN) {
+      $('#inp_kol').val(kol_chel);
+      dostup_zon(kol_chel);
+    }
+    if (Number(kod_loc != NaN)) {
+      /* $('.tab_zones article').css('display', 'none');*/
+
+      $('#loc_' + kod_loc).show();
+      /* $('#loc_NaN').hide();*/
+    }
+    //заполняем календарь
+    if (cal_inp != null) {
+      $('#inp_cal').val(cal_inp);
+    }
+  }
+});
+//перебираем ячейки таблицы сверяем значение с количеством людей
+function dostup_zon(k) {
+  let kol = Number(k);
+  if ((kol != NaN) && (kol > 0)) {
+    $(".nedostupen").each(function (index) {
+      if (Number($(this).attr('alt')) >= kol) {
+        $('#Al_' + index).addClass("dostupen").removeClass("nedostupen").text('Заказать');
+      }
+    });
+  }
+}
+//Кнопка со ссылкой для страницы booking
+
+function k_broni(i) {
+  if ($('#Al_' + i).hasClass('dostupen') && (check_select() && check_cal() && check_kol())) {
+    document.location = 'booking.html?' + 'loc=' + send_kod($('#myselect').text()) + '&kol=' + $('#inp_kol').val() + '&cal=' + $('#inp_cal').val() + '&zona=' + i;
+  }
+
+}
